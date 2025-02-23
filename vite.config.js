b70030955ccbce557eb7eabad0f9ff6ccbf2916c
yogi10723 <yogendra.singh1@cloudkb.co.uk>
2025-02-23 17:56:01 +0000

    new change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/swiftwork/',  
+  base: '/workExc/',  
   build: {
     outDir: 'dist', // This ensures the build output goes to the dist folder
   },
